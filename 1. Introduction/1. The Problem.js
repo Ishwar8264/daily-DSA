01d779349be05dd023d2445e948f5116d4bf2a02
Ishwar Sahani <ishwar.sahani119@gamil.com>
2026-03-21 20:42:18 +0530

    add one more methods

--- 1. Introduction/1. The Problem.js
+++ 1. Introduction/1. The Problem.js
@@ -12,12 +12,30 @@
 
 const studentsDatabase = ["jordan", "erick", "john", "michel"];
 
-const findStudents = (allStd, stdName) => {
-  if (!stdName) return null;
-  const stdFound = allStd.includes(stdName);
-  return stdFound ? `Found ${stdName}` : `Not found ${stdName}`;
+// 1. method
+
+// const studentName = "john";
+
+// console.log(studentName);
+
+// 2. basic loop
+
+const findStudents = (students, name) => {
+  for (let i = 0; i < students.length; i++) {
+    if (students[i] === name) {
+      return `found ${name}`;
+    }
+  }
+
+  return `not found ${name}`;
 };
 
-const results = findStudents(studentsDatabase, "Ishwar");
+// const findStudents = (allStd, stdName) => {
+//   if (!stdName) return null;
+//   const stdFound = allStd.includes(stdName);
+//   return stdFound ? `Found ${stdName}` : `Not found ${stdName}`;
+// };
+
+const results = findStudents(studentsDatabase, "john");
 
 console.log(results);
