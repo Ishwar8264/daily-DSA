f218fd09ec5da89d947de8ff8cf4cf0c191fd89f
Ishwar Sahani <ishwar.sahani119@gamil.com>
2026-03-21 20:16:49 +0530

    refactor: code and find another mmethod

--- 1. Introduction/1. The Problem.js
+++ 1. Introduction/1. The Problem.js
@@ -1,13 +1,23 @@
 // Data Structure 👇
+// const studentsDatabase = ["jordan", "erick", "john", "michel"];
+
+// // Algorithm for finding a specific user
+// const findStudent = (allStudents, studentName) => {
+//   for (let i = 0; i < allStudents.length; i++) {
+//     if (allStudents[i] === studentName) {
+//       console.log(`Found ${studentName}`);
+//     }
+//   }
+// };
+
 const studentsDatabase = ["jordan", "erick", "john", "michel"];
 
-// Algorithm for finding a specific user
-const findStudent = (allStudents, studentName) => {
-  for (let i = 0; i < allStudents.length; i++) {
-    if (allStudents[i] === studentName) {
-      console.log(`Found ${studentName}`);
-    }
-  }
+const findStudents = (allStd, stdName) => {
+  if (!stdName) return null;
+  const stdFound = allStd.includes(stdName);
+  return stdFound ? `Found ${stdName}` : `Not found ${stdName}`;
 };
 
-findStudent(studentsDatabase, "erick");
+const results = findStudents(studentsDatabase, "Ishwar");
+
+console.log(results);
